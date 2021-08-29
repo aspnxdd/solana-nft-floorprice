bcd69593363d129de9d2d5696bc33969d59c215a
aspnxdd <43625217+aspnxdd@users.noreply.github.com>
2021-08-29 17:06:25 +0200

    x

--- server/api.js
+++ server/api.js
@@ -5,6 +5,7 @@ const connection = {};
 const dev = process.env.NODE_ENV !== 'production'
 const datafetched =require("../models/datafetched");
 const server = express()
+require('dotenv').config()
 
 dbConnect();
 
