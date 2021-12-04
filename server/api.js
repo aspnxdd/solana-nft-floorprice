9d81271ec004f8b1a88b8c1ec6505c2f0e334364
aspnxdd <43625217+aspnxdd@users.noreply.github.com>
2021-12-04 18:41:12 +0100

    Update api.js

--- server/api.js
+++ server/api.js
@@ -10,7 +10,7 @@ const Redis = require("redis")
 const isDev = process.argv[2] === "--development";
 
 
-createClient({
+const redisClient = Redis.createClient({
   url: process.env.REDIS_URL
 });
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
